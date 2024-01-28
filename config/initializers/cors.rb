c31d06e1af4afc5426b96d7bb80474e6aa43e20c
Xavi122323 <xavier.ramos@udla.edu.ec>
2024-01-28 14:07:33 -0500

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200"
+    origins "http://localhost:4200", "frontend-calculo-notas.vercel.app"
 
     resource "*",
       headers: :any,
