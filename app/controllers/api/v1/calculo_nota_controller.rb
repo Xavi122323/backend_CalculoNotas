a161326fc82056351405291d0550358dbc2a603d
Xavi122323 <xavier.ramos@udla.edu.ec>
2024-02-02 21:27:31 -0500

    backend functionally update

--- app/controllers/api/v1/calculo_nota_controller.rb
+++ app/controllers/api/v1/calculo_nota_controller.rb
@@ -1,21 +1,17 @@
 class Api::V1::CalculoNotaController < ApplicationController
     def calcular_progresos
-        
         progreso1_dates = { start: params[:progreso1_start], end: params[:progreso1_end] }
         progreso2_dates = { start: params[:progreso2_start], end: params[:progreso2_end] }
         progreso3_dates = { start: params[:progreso3_start], end: params[:progreso3_end] }
-        notas_count_progreso1 = params[:notas_count_progreso1].to_i
-        notas_count_progreso2 = params[:notas_count_progreso2].to_i
-        notas_count_progreso3 = params[:notas_count_progreso3].to_i
     
-        grades = Alumno.calculoNotas(progreso1_dates, progreso2_dates, progreso3_dates, notas_count_progreso1, notas_count_progreso2, notas_count_progreso3)
+        grades = Alumno.calculate_progress_grades(progreso1_dates, progreso2_dates, progreso3_dates)
     
         render json: grades
-    end
+      end
 
     def index
         @notas = Nota.joins(:alumno).select('nota.*, alumnos.nombre as alumno_nombre')
         render json: @notas.as_json(only: [:id, :nota, :fecha, :alumno_id], methods: [:alumno_nombre])
-      end
+    end
     
 end
--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -1,23 +1,26 @@
 class Alumno < ApplicationRecord
     has_many :notas , class_name: 'Nota'
-    def self.calculoNotas(progreso1_dates, progreso2_dates, progreso3_dates, notas_count_progreso1, notas_count_progreso2, notas_count_progreso3)
-
-        return 'Invalid dates or notas counts' unless progreso1_dates[:start] < progreso1_dates[:end] && progreso1_dates[:end] < progreso2_dates[:start] && progreso2_dates[:start] < progreso2_dates[:end] && progreso2_dates[:end] < progreso3_dates[:start] && progreso3_dates[:start] < progreso3_dates[:end] && notas_count_progreso1 > 0 && notas_count_progreso2 > 0 && notas_count_progreso3 > 0
-
+    def self.calculate_progress_grades(progreso1_dates, progreso2_dates, progreso3_dates)
         Alumno.all.map do |alumno|
-
-        progreso1_grade = calculoNotasProgreso(alumno, progreso1_dates, notas_count_progreso1, 0.25)
-        progreso2_grade = calculoNotasProgreso(alumno, progreso2_dates, notas_count_progreso2, 0.35)
-        progreso3_grade = 6 - (progreso1_grade + progreso2_grade)
-
-        # Return structured results
-        { alumno_id: alumno.id, nombre: alumno.nombre, progreso1_grade: progreso1_grade, progreso2_grade: progreso2_grade, progreso3_grade: progreso3_grade }
+        
+          notas_count_progreso1 = alumno.notas.where(fecha: progreso1_dates[:start]..progreso1_dates[:end]).count
+          notas_count_progreso2 = alumno.notas.where(fecha: progreso2_dates[:start]..progreso2_dates[:end]).count
+          notas_count_progreso3 = alumno.notas.where(fecha: progreso3_dates[:start]..progreso3_dates[:end]).count
+    
+          
+          progreso1_grade = notas_count_progreso1 > 0 ? calculate_grade_for_progreso(alumno, progreso1_dates, notas_count_progreso1, 0.25) : 0
+          progreso2_grade = notas_count_progreso2 > 0 ? calculate_grade_for_progreso(alumno, progreso2_dates, notas_count_progreso2, 0.35) : 0
+          
+          progreso3_grade = 6 - (progreso1_grade + progreso2_grade)
+    
+          
+          { alumno_id: alumno.id, progreso1_grade: progreso1_grade, progreso2_grade: progreso2_grade, progreso3_grade: progreso3_grade }
         end
-    end
+      end
 
     private
 
-    def self.calculoNotasProgreso(alumno, dates, notas_count, weight)
+    def self.calculate_grade_for_progreso(alumno, dates, notas_count, weight)
         notas = alumno.notas.where(fecha: dates[:start]..dates[:end])
         total_notas = notas.sum(:nota)
         (total_notas / notas_count) * weight
